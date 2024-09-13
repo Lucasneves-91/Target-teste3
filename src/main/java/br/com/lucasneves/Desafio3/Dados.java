1fdc8355ddc41d4c0ca3aa84af33a67766ab460b
Lucas Neves <lucas.ccdr@gmail.com>
2024-09-13 11:07:49 -0300

    Incluido dados que irão ser buscado no arquivo.

---
+++ src/main/java/br/com/lucasneves/Desafio3/Dados.java
@@ -0,0 +1,30 @@
+package br.com.lucasneves.Desafio3;
+
+public class Dados {
+    private int dia;
+    private double valor;
+
+    public int getDia() {
+        return dia;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public String toString() {
+        return "Dados{" +
+                "dia=" + dia +
+                ", valor=" + valor +
+                '}';
+    }
+}
